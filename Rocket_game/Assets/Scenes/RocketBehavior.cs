8332ec35655c98e65720f236df2cec7b10d5cda2
Jp1200 <jprkr96@gmail.com>
2020-10-21 13:37:12 -0500

    audio for rocket added to prefab

--- Rocket_game/Assets/Scenes/RocketBehavior.cs
+++ Rocket_game/Assets/Scenes/RocketBehavior.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class RocketBehavior : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioS;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioS = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,7 +23,11 @@ public class RocketBehavior : MonoBehaviour
     private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space))
-        { rigidBody.AddRelativeForce(Vector3.up); }
+        {
+            if (!audioS.isPlaying) { audioS.Play(); }
+            
+            rigidBody.AddRelativeForce(Vector3.up); }
+        else { audioS.Stop(); }
         if (Input.GetKey(KeyCode.A))
         {
             //rotation = rigidBody * Time.deltaTime;
